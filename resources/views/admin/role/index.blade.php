63203dbecec50fae5d02ccc065b273513989ef5f
khmuhib <khmuhib2013@gmail.com>
2023-10-27 16:39:20 +0600

    role page deisgn

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -3,14 +3,182 @@
 @section('css')
     {{-- <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}"> --}}
 @endsection
+@section('page_title', 'Role Management')
 @section('content')
     <!-- Small boxes (Stat box) -->
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row py-2">
+        {{-- <div class="col-md-12">
             <h1>Role</h1>
+        </div> --}}
+
+        <div class="col-md-12">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Role Name</label>
+                <input type="text" class="form-control" id="" placeholder="Enter Role Name">
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card card-info">
+                <div class="card-header">
+                    <h3 class="card-title">User</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <form role="form">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!-- checkbox -->
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label" id="checkAll">All Check</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox checked</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox disabled</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <!-- checkbox -->
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox checked</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox disabled</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card card-info">
+                <div class="card-header">
+                    <h3 class="card-title">File</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <form role="form">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!-- checkbox -->
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label" id="checkAll">All Check</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox checked</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox disabled</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <!-- checkbox -->
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox checked</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox disabled</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card card-info">
+                <div class="card-header">
+                    <h3 class="card-title">Property</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <form role="form">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!-- checkbox -->
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label" id="checkAll">All Check</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox checked</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox disabled</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <!-- checkbox -->
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox checked</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox">
+                                        <label class="form-check-label">Checkbox disabled</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="d-flex justify-content-end">
+                <button type="submit" class="btn btn-secondary">Cancle</button>
+                <button type="submit" class="btn btn-primary ml-2">Submit</button>
+            </div>
         </div>
     </div>
 @endsection
 @section('js')
     {{-- <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script> --}}
+    <script>
+        $("#checkAll").click(function() {
+            $(".check").prop('checked', $(this).prop('checked'));
+        });
+    </script>
 @endsection
